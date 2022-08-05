8fbdc28717c8b4957c5ecd5c05043fe8a32e06ff
erfan <erfanbanaei110@gmail.com>
2022-08-05 06:43:37 +0430

    Convert Py To Exe

---
+++ Convert Py To Exe/main.py
@@ -0,0 +1,11 @@
+# a.py
+print("Hero Code is one of the best :)")
+input()
+
+#b.py
+import tkinter
+TK=tkinter.Tk()
+B=tkinter.Button(TK,text="Hero Code")
+TK.geometry("200×50")
+B.pack()
+TK.mainloop()
