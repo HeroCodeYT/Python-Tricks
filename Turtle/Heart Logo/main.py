621b51cba067520252cdebe2c6072c4076a881ab
erfan <erfanbanaei110@gmail.com>
2022-08-22 04:25:39 +0430

    Turtle

---
+++ Turtle/Heart Logo/main.py
@@ -0,0 +1,23 @@
+# Hero Code
+import turtle
+pen = turtle.Turtle()
+
+def curve():
+    for i in range(200):
+        pen.right(1)
+        pen.forward(1)
+  
+
+def heart():
+    pen.fillcolor('red')
+    pen.begin_fill()
+    pen.left(140)
+    pen.forward(113)
+    curve()
+    pen.left(120)
+    curve()
+    pen.forward(112)
+    pen.end_fill()
+
+heart()
+pen.ht()
